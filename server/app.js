4c7bfb027d257596f4b39f2f3705c57a7468a5da
Álvaro Valle Melgarejo <alvaro.valle.fullstack@outlook.com>
2026-01-20 16:36:41 +0100

    primer server

---
+++ server/app.js
@@ -0,0 +1,47 @@
+import createError from 'http-errors';
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+// import indexRouter from './routes/index.js';
+import userRouter from './modules/userModules/user.routes.js';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+
+dotenv.config();
+
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(cors({
+  origin: process.env.FRONT_URL
+}))
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//app.use('/', indexRouter);
+app.use('/user', userRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+export default app;
---
+++ server/config/db.js
@@ -0,0 +1,40 @@
+import sql from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const dbPool = sql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  datestrings: true
+})
+
+const executeQuery = async (sql, values = []) => {
+
+  let connection;
+
+  try {
+    // abrimos conexión con la pool
+
+    connection = await dbPool.getConnection();
+    
+    // Ejecutamos la petición
+    const [result] = await connection.query(sql, values);
+
+    return result;
+    
+  } catch (error) {
+    console.log('Error en la consulta', error);
+    throw error;
+  }
+  finally {
+    // Cerramos la conexión
+    if (connection) {
+      connection.release();
+    }
+  }
+} 
+
+export default executeQuery;
