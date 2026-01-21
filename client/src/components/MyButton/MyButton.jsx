80d4285246bd7200443dc3abd18a4a27f6b372d5
Álvaro Valle Melgarejo <alvaro.valle.fullstack@outlook.com>
2026-01-21 15:20:54 +0100

    cambios

---
+++ client/src/components/MyButton/MyButton.jsx
@@ -0,0 +1,7 @@
+import {Button} from 'react-bootstrap';
+
+export const MyButton = ({text, onSubmit, btnClass}) => {
+  return (
+    <Button className={btnClass} onClick={onSubmit} >{text}</Button>
+  )
+}
