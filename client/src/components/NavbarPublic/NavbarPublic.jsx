f896ae96210203eff734e605e5b48b78023300e2
Loli Mariscal <lolimg_jinete@hotmail.com>
2026-01-27 17:32:36 +0100

    fixed

---
+++ client/src/components/NavbarPublic/NavbarPublic.jsx
@@ -0,0 +1,46 @@
+import './NavbarPublic.css';
+import { MyButton } from '../../components/MyButton/MyButton.jsx';
+
+
+export const NavbarPublic = ({ setShowPage, showPage }) => {
+
+
+  return (
+    <div className='header-public'>
+      <div>
+        <img id='inicio' src="/src/assets/Images/logo/logo.png" />
+      </div>
+      <div className='buttons-cont'>
+        {/* Botones que redirigen de una sección a otra del home */}
+        <MyButton
+          text='Inicio'
+          onSubmit={() => document.getElementById('inicio').scrollIntoView({ behavior: 'smooth' })}
+          btnClass='btn-white'
+        />
+        <MyButton
+          text='Características'
+          onSubmit={() => document.getElementById('caracteristicas').scrollIntoView({ behavior: 'smooth' })}
+          btnClass='btn-white'
+        />
+        <MyButton
+          text='Contacto'
+          onSubmit={() => document.getElementById('contacto').scrollIntoView({ behavior: 'smooth' })}
+          btnClass='btn-white'
+        />
+        {showPage === "register" ?
+          <MyButton
+            text='Iniciar sesión'
+            onSubmit={() => setShowPage('login')}
+            btnClass='btn-green'
+          />
+          :
+          <MyButton
+            text='Registrarse'
+            onSubmit={() => setShowPage('register')}
+            btnClass='btn-green'
+          />
+        }
+      </div>
+    </div>
+  )
+}
