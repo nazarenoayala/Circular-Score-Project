85c7c40dc54197b7d4a1738619f96a38df83bcc1
Daniel Pérez <daniel.mailprof@gmail.com>
2026-01-30 13:00:05 +0100

    navegación con discriminaciones según tipo de user

--- client/src/routes/PrivateRoutes.jsx
+++ client/src/routes/PrivateRoutes.jsx
@@ -1,9 +1,15 @@
-import { Outlet } from 'react-router';
+import { useEffect } from 'react';
+import { Outlet, useNavigate } from 'react-router';
 
+export const PrivateRoutes = ({userData, requiredType}) => {
 
-
-export const PrivateRoutes = () => {
+  const navigate = useNavigate();
+  console.log("USERTYPE Y USER DATA EN PRIVATE ROUTES", userData, requiredType);
+  console.log(requiredType);
   
+  useEffect(()=> {
+    if(userData?.type !== requiredType) navigate("/");
+  },[userData]);
 
   return (
     <>
