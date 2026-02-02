2327d6802ace82969d79fadfb9b6273b83c23d20
susii-01 <s.rodriguez4a@gmail.com>
2026-02-02 11:53:43 +0100

    Update InfoRepresentante.jsx

--- client/src/components/InfoRepresentante/InfoRepresentante.jsx
+++ client/src/components/InfoRepresentante/InfoRepresentante.jsx
@@ -1,12 +1,15 @@
 import './InfoRepresentante.css'
+import { question3 } from '../../data/CompanyRegisterData/Question3'
 
 export const InfoRepresentante = ({company}) => {
 
- 
+  //mapeo del archivo question3.js
+ const position = Object.fromEntries(question3.map(e => [e.id, e.name]));
+
   return (
     <div className='contact_info'>
       <h3><span>Persona de Contacto:</span> {company?.company.name}</h3>
-      <h3><span>Ocupación:</span> {company?.company.position}</h3>
+      <h3><span>Ocupación:</span> {position[company?.company.position]}</h3>
       <h3><span>Teléfono:</span> {company?.company.phone_number}</h3>
       <h3><span>Email:</span> {company?.company.user_email}</h3>      
     </div>
