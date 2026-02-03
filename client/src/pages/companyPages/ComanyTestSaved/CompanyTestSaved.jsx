08ec7c4d1e94c3a8b57159890f55c67f773c205f
yasmine <adianiepene@yahoo.fr>
2026-02-03 20:27:45 +0100

    savetest

--- client/src/pages/companyPages/ComanyTestSaved/CompanyTestSaved.jsx
+++ client/src/pages/companyPages/ComanyTestSaved/CompanyTestSaved.jsx
@@ -4,164 +4,151 @@ import { AuthContext } from '../../../context/AuthContext/AuthContext';
 import { Link, useNavigate, useParams } from 'react-router';
 import { MyButton } from '../../../components/MyButton/MyButton';
 import './CompanyTestSaved.css';
+import options from '../../../data/CompanyRegisterData/options';
 
 import { QuestionAnswerCard } from '../../../components/QuestionAnswerCard/QuestionAnswerCard';
 const urlImage = import.meta.env.VITE_IMAGES;
 
 const CompanyTestSaved = () => {
-const navigate = useNavigate();
+  const navigate = useNavigate();
 
   // Nos traemos el array de los test
-  const { test, token } = useContext(AuthContext);
+  const { test, token, companyData } = useContext(AuthContext);
+  console.log('', companyData);
 
   //Rescatar id del test
-  const { id , answer_set_id } = useParams();
+  const { id, answer_set_id } = useParams();
   //Filtrar el test que queremos a través del id rescatado
-  const uniqueTest = test?.find((e) => e.test_id == id);
-
-  const [loading, setLoading] = useState(true);
-
-  //Array traído del back
-  const [AllTestsCompanies, setAllTestsCompanies] = useState([]);
-
-  // Seteamos un marcador de pregunta
-  const [index, setIndex] = useState(0);
+  const currentTest = test?.find((e) => e.test_id == id);
 
   // Creamos un estado para las respuestas:
-  const [answer, setAnswer] = useState({});
-
+  const [answersTorender, setAnswersTorender] = useState([]);
   // Guardamos en este estado el array de preguntas que nos traemos de BD
   const [questions, setQuestions] = useState([]);
 
-  useEffect(() => {
-    //info de la empresa
-    const fetchAllTestsCompanies = async () => {
-      try {
-        let result = await fetchData(`/company/allCompaniesData/${id}`,'GET',null,token,);
-        setAllTestsCompanies(result.data.result);
-        console.log(result);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-     fetchAllTestsCompanies();
-   
+  console.log(questions);
 
+  useEffect(() => {
     //preguntas del test
     const fetchQuestions = async () => {
       try {
-        let result = await fetchData(`/question/getQuestions/${id}`,'GET',null,token);
+        let result = await fetchData(
+          `/question/getQuestions/${id}`,
+          'GET',
+          null,
+          token,
+        );
         const allQ = result.data.result;
-        setQuestions(allQ);
+     console.log(allQ);
+     
+     
+        let result2 = await fetchData(
+          `/answer/savedAnswers/${answer_set_id}`,
+          'GET',
+          null,
+          token,
+        );
+       // console.log(result2);
+        const allanswers = result2.data.result
+        console.log(allanswers);
+        
+        const finalData = allanswers.map((a)=> {
+           const question = allQ.find(q=> q.question_id === a.question_id)
+           const opts = options.find(o=> o.value === a.user_answer )
+      
+           return {
+            question_id: question?.question_id,
+            question: question?.text, 
+            answer: opts?.name
+           }
+          
+        })
+        setAnswersTorender(finalData)
+       console.log(finalData);
+     
+       
       } catch (error) {
         console.log(error);
       }
     };
-      fetchQuestions();
- 
+    fetchQuestions();
+  }, []);
 
+
+  console.log(answersTorender);
+  
+  useEffect(() => {
     //respuestas guardadas
     const fetchAnswers = async () => {
-      // Si answer_set_id no tiene valor o es  "undefined", salimos de la función
-     
       try {
-        let result = await fetchData(`/answer/savedAnswers/${id}`,'GET',null,token,);
-     
-    
+        
+
+
         //verificamos que existe la respuesta
-         if (result && result.data && result.data.result) {
+        //  if (result && result.data && result.data.result) {
 
-          //objeto para guardar las respuetas
-          const savedAnswers = {};
-          for (let i = 0; i < result.data.result.length; i++) {
-          const item = result.data.result[i]; 
-          savedAnswers[item.question_id] = item.user_answer;
-          }
+        //   //objeto para guardar las respuetas
+        //   const savedAnswers = {};
+        //   for (let i = 0; i < result.data.result.length; i++) {
+        //   const item = result.data.result[i];
+        //   savedAnswers[item.question_id] = item.user_answer;
+        //   }
 
-          setAnswer(savedAnswers);
-          const answerLength = result.data.result.length;
-          setIndex(answerLength);
-        }
+        //   setAnswers(savedAnswers);
+        //   const answerLength = result.data.result.length;
+        //   setIndex(answerLength);
+        // }
 
-        setLoading(false);
+        // setLoading(false);
       } catch (error) {
         console.error(error);
         setLoading(false);
       }
     };
-      fetchAnswers();
-   
-  }, [id, token]);
+    fetchAnswers();
+  }, []);
 
-          const continueTest = () =>{
+  // }, [id, token]);
 
-           navigate(`/newTest/${id}?answer_set_id=${answer_set_id}`); 
-           
-        };
+  const continueTest = () => {
+    navigate(`/newTest/${id}?answer_set_id=${answer_set_id}`);
+  };
 
- 
+  return (
+    <div>
+      <div>
+        <div>
+          <h2>nombres companias:{companyData.name}</h2>
+          <h2>nombre del test: {currentTest.test_name}</h2>
+          <img src={`${urlImage}/ODSimages/${currentTest.test_image}`} alt="" />
+        </div>
+      </div>
+      <div>
+       {answersTorender.map(elem => {
+        return (
+          <div>
+             <p>{elem.question_id}: {elem.question}</p>
+             <p>{elem.answer}</p>
+             <hr />
 
   
-  return (
-   <div>
-  <div >
-  <div>
-    {AllTestsCompanies?.map((elem) => (
-      <div className="test-card" key={elem.company_id}>
-    
-        <div className="test-info">
-          <h4 className="company-name">
-            Nombre de la empresa:{' '}
-            <Link
-              to={`/oneCompany/${elem.user_id}`}
-              className="myLink"
-            >
-              {elem.company_name}
-            </Link>
-          </h4>
+          </div>
+       
+        )
+       })
 
-          <h4 className="test-name">
-            Test: {uniqueTest?.test_name}
-          </h4>
-        </div>
-
-        <div className="test-image">
-          <img
-            src={`${urlImage}/ODSimages/${uniqueTest?.test_image}`}
-            alt={uniqueTest?.test_name}
-          />
-        </div>
-
+       }
       </div>
-    ))}
-  </div>
-</div>
-  <div className="p-4">
-    {!loading && questions.length > 0 && (
-      <div>
-        <div className="mb-5">
-          {questions
-            .filter((q) => answer.hasOwnProperty(q.question_id))
-            .map((pregunta) => (
-              <QuestionAnswerCard
-                key={pregunta.question_id}
-                question={pregunta}
-                answer={answer}
-              />
-            ))}
-        </div>
-
-        <div>
+      <div className="p-4">
+    
           <MyButton
             text="Continuar Test"
             btnClass="btn-green"
             onSubmit={continueTest}
           />
         </div>
-      </div>
-    )}
-  </div>
-</div>
-  )
-}
+     
+    </div>
+  );
+};
 export default CompanyTestSaved;
