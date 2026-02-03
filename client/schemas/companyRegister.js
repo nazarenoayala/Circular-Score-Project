80c9ca4385cc3f788b6eb443001f319d8b5f5164
Daniel Pérez <daniel.mailprof@gmail.com>
2026-02-03 12:35:21 +0100

    updates

--- client/schemas/companyRegister.js
+++ client/schemas/companyRegister.js
@@ -42,7 +42,7 @@ export const companyRegisterSchema = z.object({
             .min(1, 'El email es obligatorio')
             .email('Debes introducir un email válido'),
 
-  contact_name: z.string()
+  name: z.string()
                      .min(2, 'El nombre debe contener al menos 2 caracteres')
                      .max(50, 'El nombre debe contener como máximo 50 caracteres')
                      .regex(/^[A-Za-zÁÉÍÓÚáéíóúÑñÜü\s]+$/, 'El nombre no puede contener ni números ni carácteres especiales'), 
